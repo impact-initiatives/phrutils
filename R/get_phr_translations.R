71b57fd02c86298eb645c95bf9fdcf06502ad3a1
SaeedR1987 <saeed.rahman0@gmail.com>
2026-08-30 08:09:49 +0200

    commit 30082026

--- R/get_phr_translations.R
+++ R/get_phr_translations.R
@@ -1,7 +1,9 @@
 get_phr_translations <- function() {
+
   if (is.null(.phr_env$translations)) {
-    phr_init_translations()
+    .phr_env$translations <- phr_load_translations()
   }
 
   .phr_env$translations
 }
+``
